3604e066cd289c4a18c5a7a2735c083c96fd192a
Jim Van Fleet <jim@jimvanfleet.com>
2023-02-15 14:54:36 -0500

    I saw some spans and traces!

--- course-files/tracing-extra/app/app.py
+++ course-files/tracing-extra/app/app.py
@@ -6,10 +6,10 @@ from flask import Flask, jsonify
 
 import logging
 from jaeger_client import Config
-from flask_opentracing import FlaskTracing
+from flask_opentracing import FlaskTracer
 
 import redis
-import redis_opentracing
+#import redis_opentracing
 
 app = Flask(__name__)
 
@@ -21,19 +21,24 @@ def init_tracer(service):
     logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
     config = Config(
-        config={"sampler": {"type": "const", "param": 1,}, "logging": True,},
-        service_name=service,
-    )
+      config={
+        'sampler':
+          {
+            'type': 'const', 'param': 1
+          },
+          'logging': True,
+          'reporter_batch_size': 1,
+      }, 
+      service_name="service")
 
     # this call also sets opentracing.tracer
     return config.initialize_tracer()
 
-
-# starter code
 tracer = init_tracer("test-service")
 
 # not entirely sure but I believe there's a flask_opentracing.init_tracing() missing here
-redis_opentracing.init_tracing(tracer, trace_all_classes=False)
+flask_tracer = FlaskTracer(tracer, True, app)
+#redis_opentracing.init_tracing(tracer, trace_all_classes=False)
 
 with tracer.start_span("first-span") as span:
     span.set_tag("first-tag", "100")
@@ -46,21 +51,23 @@ def hello_world():
 
 @app.route("/alpha")
 def alpha():
-    for i in range(100):
-        do_heavy_work()  # removed the colon here since it caused a syntax error - not sure about its purpose?
-        if i % 100 == 99:
-            time.sleep(10)
-    return "This is the Alpha Endpoint!"
+    with tracer.start_span("alpha") as span:
+      for i in range(100):
+          with tracer.start_span('alpha-heavy', child_of=span) as child_span:
+            if i % 100 == 99:
+                time.sleep(10)
+      return "This is the Alpha Endpoint!"
 
 
 @app.route("/beta")
 def beta():
-    r = requests.get("https://www.google.com/search?q=python")
-    dict = {}
-    for key, value in r.headers.items():
+    with tracer.start_span("beta") as span:
+      r = requests.get("https://www.google.com/search?q=python")
+      dict = {}
+      for key, value in r.headers.items():
         print(key, ":", value)
         dict.update({key: value})
-    return jsonify(dict)
+      return jsonify(dict)
 
 
 @app.route(
@@ -68,7 +75,7 @@ def beta():
 )  # needed to rename this view to avoid function name collision with redis import
 def writeredis():
     # start tracing the redis client
-    redis_opentracing.trace_client(rdb)
+    #redis_opentracing.trace_client(rdb)
     r = requests.get("https://www.google.com/search?q=python")
     dict = {}
     # put the first 50 results into dict
